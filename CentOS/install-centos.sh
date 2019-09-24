216be692945fcc83649397ec973f09f496a08313
roybhaskar9 <bhaskar@study9.com>
2019-09-24 08:00:42 +0530

    Update install-centos.sh

--- CentOS/install-centos.sh
+++ CentOS/install-centos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# We are installing the software repository for kubernetes
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -10,13 +10,15 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-
+# Now we are installing kubeadm and docker
 yum install -y net-tools screen tree telnet kubelet kubeadm docker --nogpgcheck
+# Below command will start docker and kubernetes agent i.e. kubelet on every system reboot
 systemctl enable kubelet && systemctl start kubelet
 systemctl enable docker && systemctl start docker
-
+# We need to disable SELINUX and SWAP permanently
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 swapoff -a
 sed -i '/swap/s/^/#/g' /etc/fstab
+# Below command will fix a network issue with bridged adapter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
